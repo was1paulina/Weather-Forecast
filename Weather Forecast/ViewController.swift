fece51954468a5e07771374afda98430d328b7b0
was1paulina <was1paulina@gmail.com>
2016-07-10 19:20:35 -0700

    Initial Commit

---
+++ Weather Forecast/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather Forecast
+//
+//  Created by Palina Tsaryk on 7/10/16.
+//  Copyright © 2016 was1paulina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
